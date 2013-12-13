1f216c235c1f750515fe2f4caf025b12aa6a69b3
twurz <travis.wurzer@yahoo.com>
2013-12-13 11:05:49 -0800

    ex1.rb

---
+++ ex21.rb
@@ -0,0 +1,35 @@
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  a / b
+end
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 1)
+height = subtract(78,9)
+weight = multiply(70, 3)
+iq = divide(280, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what} Can you do it by hand?"
